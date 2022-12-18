c8038e12c592c547cbc9bd5167edc59e15a169e5
dc-choi <cheek0805@naver.com>
2022-12-19 00:00:00 +0900

    C++ 기본 개념 학습

---
+++ alg_lecture/define.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define PI 3.14159
+#define loop(x, n) for(int x = 0; x < n; x++)
+/**
+ * define이란?
+ * 상수나 매크로를 정의하는 부분을 말한다.
+ * 
+ * 여기서는 PI를 상수로 지정하고 반복문을 매크로로 정의했다.
+*/
+
+int main() {
+	cout << PI << '\n';
+	int sum = 0;
+
+	loop(i, 10){
+		sum += i;
+	}
+	// for (int i = 0; i < 10; i++) {
+	// 	sum += i;
+	// }
+
+	cout << sum << '\n';
+	return 0;
+}
---
+++ alg_lecture/stl.cpp
@@ -0,0 +1,34 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	/**
+	 * C++은 STL (Standard Template Library)을 제공한다.
+	 * 이는 자료구조, 함수등을 제공하는 라이브러리이다.
+	 * 알고리즘, 컨테이너, 이터레이터, 펑터 이렇게 4가지를 제공합니다.
+	 * 
+	 * 우리가 C++로 vector 자료구조를 쓴다던가 sort() 함수를 쓸 수 있는 것은 다 STL덕입니다.
+	 * 
+	 * 알고리즘
+	 * 정렬, 탐색등에 관한 함수로 이루어져 있습니다. sort()가 대표적입니다.
+	 * 
+	 * 컨테이너
+	 * 여러가지 의미로 쓰입니다. 여기서는 자료구조라는 의미입니다.
+	 * 
+	 * 시퀀스 컨테이너(sequence container): 데이터를 단순히 저장해놓은 자료구조
+	 * ex) array, vector, deque, forward_list, List
+	 * 
+	 * 연관 컨테이너(associative container): 자료가 저장됨에 따라 자동 정렬되는 자료구조, 중복키가 가능한 것은 이름에 multi가 붙습니다.
+	 * ex) set, map, multiset, multimap이 있습니다.
+	 * 
+	 * 정렬되지 않은 컨테이너(unordered associative container): 자료가 저장됨에 따라 자동정렬이 되지 않는 자료구조
+	 * ex) unordered_set, unordered_map, unordered_multiset, unordered_multimap
+	 * 
+	 * 컨테이너 어댑터(container adapter): 시퀀스 컨테이너를 이용해 만든 자료구조
+	 * ex) stack, queue => deque,  priority_queue => vector
+	 * 
+	 * 나중에 자료구조에 대해서 자세히 배웁니다.
+	 * 이터레이터도 나중에 배웁니다.
+	 * 일단은 넘어갑니다.
+	*/
+	return 0;
+}
